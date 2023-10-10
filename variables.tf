fdc1c98e8dd483103d1eae1b7511351ac72844cb
Raffs <ajrafferty67@gmail.com>
2023-10-10 11:15:16 +1100

    moved admin_ssh_key var to secrets.tf ( not xfrd to git)

--- variables.tf
+++ variables.tf
@@ -32,12 +32,6 @@ variable "admin_user" {
   default     = "adminuser"
 }
 
-variable "admin_ssh_key" {
-  description = "SSH public key for VM access"
-  type        = string
-  default     = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQClqyrrOtR4olHb/J/NBUKaeVlyxREsTCvtVlH/PqQDCZ933LpzXLZS5Xf03KookQJR6oceoNQaCN03QHJomYtsVzF3dYbRQJnKF4Igwy4NcIxPukHSbr8Tx1pQyp1vkQUPlm6l4REdICU2PHStQVD70B+BbkMEV0ZPed9vjdcutqCK/MW4dXtUFUNGr5Cy4M/0win95NmmXy5wzfuAQHedtOAjZQk17XdClddl0TG/sMzDjHRiVpEMqNPIejSyZhv+DRUrvdLEpt2gTaSpVYd37nUkTupkt/jDDdUEaN0gDaXM2khqIJ9lyNg+zsi4SlYny6C9/byDTNlQs+CXmFXefA8NKBq5JNRuUJfEFpv1La4Sap6zOY/D3bmaZefha3XkE9iUjsj0Tug1wdWI3C51WfuRKvNGPRTPIFqtGMcVa0qRpasDCDQhH7BmOr0Skg+CVQVpJuExEABOt3Ai9KDUgRrimdxKLh+jr393P13/oa1RUon+0HwLWCkLBONXYeTfFVRFg3kAfjJP5akQoIMHIsjtMUDUM0ltI14G/h9nIedBC9mIF8Z3i54JQTFFfuyBm9Vh61myZoR48g9DO0xDRPCCDXYWLrOMdt6CaFI1pbtTybcbD8o0PLupHbrhONjejfe4e+LRpVyDEn4Kifri9cCJDAGP8UxaUDoIN2/3Ow== ajrafferty67@gmail.com"
-}
-
 variable "address_space" {
     description = "list of subnet ip address ranges"
     type = list(string)
